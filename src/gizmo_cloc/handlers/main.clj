cbe4053a3961b0e5997b2db89a2545937064c56f
Alex Petrov <alexp@coffeenco.de>
2013-09-06 11:16:20 +0200

    Simplify the name snippet selectors

--- src/gizmo_cloc/handlers/main.clj
+++ src/gizmo_cloc/handlers/main.clj
@@ -3,15 +3,18 @@
 (defn index
   [request]
   {:render :html
-   :widgets {:left-content 'gizmo-cloc.widgets.main/empty-widget
-             :main-content 'gizmo-cloc.widgets.main/index-content}})
+   :widgets {:left-content 'gizmo-cloc.widgets.main/index-content
+             :middle-content 'gizmo-cloc.widgets.main/empty-widget
+             :main-content 'gizmo-cloc.widgets.main/empty-widget
+             }})
 
 (defn library-show
   [request]
   {:render :html
    :library-name (get-in request [:route-params :library])
    :widgets {:left-content 'gizmo-cloc.widgets.main/index-content
-             :main-content 'gizmo-cloc.widgets.main/library-namespaces}})
+             :middle-content 'gizmo-cloc.widgets.main/library-namespaces
+             :main-content 'gizmo-cloc.widgets.main/empty-widget}})
 
 (defn namespace-show
   [request]
@@ -19,4 +22,6 @@
    :library-name (get-in request [:route-params :library])
    :namespace (get-in request [:route-params :namespace])
    :widgets {:left-content 'gizmo-cloc.widgets.main/index-content
-             :main-content 'gizmo-cloc.widgets.main/library-namespace-docs}})
+             :middle-content 'gizmo-cloc.widgets.main/library-namespaces
+             :main-content  'gizmo-cloc.widgets.main/library-namespace-docs
+             }})
--- src/gizmo_cloc/snippets/main.clj
+++ src/gizmo_cloc/snippets/main.clj
@@ -1,29 +1,57 @@
 (ns gizmo-cloc.snippets.main
   (:require [net.cgrand.enlive-html :as html]
-            [gizmo-cloc.routes :as routes]))
+            [gizmo-cloc.routes :as routes]
+            [clojurewerkz.gizmo.widget :refer [defselector]]))
 
-(html/defsnippet index-snippet "templates/main/index.html"
-  [:ul#libraries]
+(defn within
+  [parent & children]
+  (if (sequential? parent)
+    (apply conj parent children)
+    (apply conj [parent] children)))
+
+(defn has-attr
+ [attr]
+ (html/pred #(not (nil? (-> % :attrs attr)))))
+
+(defn snip
+  [snippet-name]
+  (html/pred #(= snippet-name (-> % :attrs :snippet))))
+
+(defn format-selector
+  [name]
+  (format "*%s" name))
+
+(defmacro defsnippet-wrapper
+ "Define a named snippet -- equivalent to (def name (snippet source selector args ...))."
+ [name source selector args & forms]
+ (let [snippets (html/select (html/html-resource source) [(has-attr :snippet)])
+       names    (apply concat (map #(vector
+                                     (-> % :attrs :snippet format-selector symbol)
+                                     (list 'snip (-> % :attrs :snippet))) snippets))]
+   `(let* [~@names]
+          (def ~name (html/snippet ~source ~selector ~args ~@forms)))))
+
+(defsnippet-wrapper index-snippet "templates/main/index.html"
+  [*libraries-list]
   [libraries]
-  [:ul#libraries [:li html/first-of-type]]
+  [*libraries-list-item]
   (html/clone-for [library libraries]
                   [html/any-node] (html/replace-vars {:library-path (routes/library-path :library library)
-
                                                       :library-name library})))
 
-(html/defsnippet library-namespaces-snippet "templates/main/index.html"
-  [:ul#namespaces]
+(defsnippet-wrapper library-namespaces-snippet "templates/main/index.html"
+  [*namespaces-list]
   [{:keys [library-name namespaces]}]
-  [:ul#namespaces [:li html/first-of-type]]
+  [*namespaces-list-item]
   (html/clone-for [namespace namespaces]
                   [html/any-node] (html/replace-vars {:namespace-path (routes/namespace-path :library library-name
                                                                                              :namespace namespace)
                                                       :namespace-name namespace})))
 
-(html/defsnippet library-namespace-docs-snippet "templates/main/index.html"
-  [:div#namespace-docs]
+(defsnippet-wrapper library-namespace-docs-snippet "templates/main/index.html"
+  [*namespace-docs]
   [{:keys [library-name namespace docs]}]
-  [:ul [:li html/first-of-type]]
+  (within *docstring-list [*docstring-list-item])
   (let [{:keys [publics doc]} docs]
     (html/clone-for [{:keys [doc file name]} publics]
                     [html/any-node] (html/replace-vars
